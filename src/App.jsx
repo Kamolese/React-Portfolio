f02e934e6379968295f95f424cf79b29a9f61600
Kamolese <vinicamolese777@gmail.com>
2024-12-12 16:23:20 -0300

    About

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 
-import styles from './App.module.css'
+import styles from "./App.module.css";
+import { Hero } from "./componets/Hero/Hero";
+import { NavBar } from "./componets/Navbar/Navbar";
+import { About } from "./components/About/About";
 
 function App() {
- return <div className={styles.App}>
- <Navbar />
+ return (
+     <div className={styles.App}>
+ <NavBar />
  <Hero />
+ <About />
  </div>
+ );
 }
 
 export default App
---
+++ src/components/About/About.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils"
+
+export const About = () => {
+    return (
+        <section className={styles.container} id="about">
+            <h2 className={styles.title}>About</h2>
+            <div className={styles.content}>
+                <img
+                 src={getImageUrl("about/aboutImage.png")}
+                 alt=""
+                 className={styles.aboutImage}
+                 />
+                 <ul className={styles.aboutItems}>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/cursoIcon.png")} alt="Cursor icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>Frontend</h3>
+                            <p>Developer with experience modern UI design.
+                            Crafting intuitive and responsive user interfaces.
+                            </p>
+                        </div>
+                 </li>
+                 <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>Backend</h3>
+                            <p>Building scalable and efficient server-side solutions.
+                            </p>
+                        </div>
+                 </li>
+                 <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/UiIcon.png")} alt="UI icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>UI Designer</h3>
+                            <p>UI Designer focused on creating intuitive,
+                                 user-friendly, and visually appealing interfaces. 
+                            </p>
+                        </div>
+                 </li>
+                 </ul>
+            </div>
+        </section>
+    )
+}
